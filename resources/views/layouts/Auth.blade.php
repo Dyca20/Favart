b53819c784641d8ef4da259d7f69e1f0f8d22ee1
Dyca20 <dilan.castro@grupobabel.com>
2022-07-15 19:52:13 -0600

    Validaciones en el registrar

--- resources/views/layouts/Auth.blade.php
+++ resources/views/layouts/Auth.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'FavArt') }}</title>
+    <link href="/css/style.css" rel="stylesheet">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
